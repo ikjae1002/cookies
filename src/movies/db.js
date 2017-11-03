8ffa6bcd6ec87a10bdb6aab5a503ce20e1d52f83
ikjae1002 <ij374@nyu.edu>
2017-11-03 10:03:32 -0400

    getting mongo set up but was not able to run with mongo

--- src/movies/db.js
+++ src/movies/db.js
@@ -1,1 +1,24 @@
 // db.js
+// as always, require the module
+const mongoose = require('mongoose'); 
+
+// some extra stuff goes here...
+
+// define the data in our collection
+// const Movie = new mongoose.Schema({
+//   Title: String,
+//   Author: String,
+//   Age: Date,
+//   Publisher: String,
+//   Reviews:[
+//       {Rating: Number, Comments: String}
+//   ]
+//});
+
+// "register" it so that mongoose knows about it
+//mongoose.model('Movie', Movie);
+
+
+
+// connect to the database (catdb)
+mongoose.connect('');
